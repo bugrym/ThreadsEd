2ba47061c2a5ec01aaa94e690edd378f096c3220
vbugrym <vbugrym@lohika.com>
2023-11-29 10:37:23 +0200

    Initial Commit

---
+++ ThreadsEd/ThreadsEdApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThreadsEdApp.swift
+//  ThreadsEd
+//
+//  Created by vbugrym on 29.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct ThreadsEdApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
